b36fe20646b75b10478d5ef5624e612dc92c3f4a
ess_ess <sravans2011@gmail.com>
2017-01-17 22:38:06 +0530

    Write tests in chai and mocha

---
+++ test.js
@@ -0,0 +1,62 @@
+var assert = require('chai').assert
+
+var deb = require('./');
+
+describe('Simple Debounce', function() {
+  describe('Sample', function() {
+    it('should run only once in 500 ms', function(done) {
+      var val = 200;
+      var debouncedFn = deb.debounce(function() {
+        val += 1;
+        done(assert.equal(val, 201));
+      }, 501);
+
+      for(var  i = 0; i < 5; i++) {
+        setTimeout(function() {
+          debouncedFn();
+        }, i * 100);
+      }
+    });
+  });
+});
+
+describe('Throttled Debounce', function() {
+  describe('Throttled Debounce Assertion', function() {
+    it('should run at start and end', function(done) {
+      var val;
+      var debouncedFn = deb.throttledDebounce(function() {
+        val = 0;
+      }, function() {
+        val += 1;
+        done(assert.equal(val, 1));
+      }, 500);
+
+      for(var  i = 0; i < 5; i++) {
+        setTimeout(function() {
+          debouncedFn();
+        }, i * 100);
+      }
+    });
+  });
+});
+
+describe('Reduced Debounce', function() {
+  describe('Reduced Debounce Assertion', function() {
+    it('should run at every iteration', function(done) {
+      var val;
+      var debouncedFn = deb.reducedDebounce(function() {
+        val = 0;
+      }, function() {
+        val += 1;
+      }, function() {
+        done(assert.equal(val, 4));
+      }, 500);
+
+      for(var  i = 0; i < 5; i++) {
+        setTimeout(function() {
+          debouncedFn();
+        }, i * 100);
+      }
+    });
+  });
+});
